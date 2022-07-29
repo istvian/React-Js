22de1644be086fa5cc463f46009ad491cd485dfa
stvian <76795576+istvian@users.noreply.github.com>
2022-07-29 15:21:09 -0400

    fix

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import "./NavBar.css";
+import CartWidget from "../CartWidget/CartWidget";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__container">
+        <Link to="/">
+          <img className="nav__logo" alt="Logo de la empresa" src="../nantiki.png" />
+        </Link>
+      </div>
+      <div className="nav__links">
+        <Link className="nav__btn" to="/category/accesorios">
+          Accesorios
+        </Link>
+        <Link className="nav__btn" to="/category/ordenadores">
+          Ordenadores
+        </Link>
+        <Link className="nav__btn" to="/category/controles">
+          Controles
+        </Link>
+        <Link className="nav__btn" to="/category/consolas">
+          Consolas
+        </Link>
+      </div>
+      <CartWidget />
+    </nav>
+  );
+};
+
+export default NavBar;
